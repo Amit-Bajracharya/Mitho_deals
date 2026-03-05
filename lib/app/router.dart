4279ce7bff0059f7d646f038ea8a316fc947ff65
Amit Muni Bajracharya <amitbajracharya4444@gmail.com>
2026-03-05 17:30:22 +0545

    created a flutter project

---
+++ lib/app/router.dart
@@ -0,0 +1,6 @@
+import 'package:go_router/go_router.dart';
+
+class MyAppRouter{
+  GoRouter routes = GoRouter(
+    routes: GoRoute(path: path) );
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MithoDeals());
+}
+
+
+class MithoDeals extends StatelessWidget {
+  const MithoDeals({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,,
+    );
+  }
+}
